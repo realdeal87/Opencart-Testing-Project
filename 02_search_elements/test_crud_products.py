4f136f4df2e06c9215e4dd3f43ae3a9b353cb2f1
Andrey <realdeal87@protonmail.com>
2019-09-12 23:38:44 +0300

    Add Excercise 8

--- 02_search_elements/test_crud_products.py
+++ 02_search_elements/test_crud_products.py
@@ -17,7 +17,7 @@ def search_product(table, name, edit=None):
 
 
 def test_create_product(sign_in):
-    """Функция добавляет продукт с заполнением минимального количества атрибутов"""
+    """Успешное добавление продукта с заполнением минимального количества атрибутов"""
     p_name = "Alcatel OT-890"
     p_meta_teg_title = "Android 2.1"
     p_model = "Product 2011"
@@ -32,16 +32,16 @@ def test_create_product(sign_in):
 
 
 def test_edit_product(sign_in):
-    """Функция редактирует первый найденный продукт по заданному названию,
-    добавляя описание продукта"""
+    """Успешное редактирование первого найденного продукта по заданному названию,
+    с добавлением описания продукта"""
     p_name = "Alcatel OT-890"
     p_description = "Alcatel OT-890 supports GSM frequency. Official announcement date is " \
-                       "February 2011. The device is working on an Android OS, v2.1 (Eclair) " \
-                       "with a 420 MHz processor. Alcatel OT-890 has 150 MB of built-in memory. " \
-                       "This device has a Mediatek MT6516 chipset. The main screen size is 2.8 " \
-                       "inches with 320 x 240 pixels resolution. It has a 143 ppi pixel density. " \
-                       "The screen covers about 38.1% of the device's body. " \
-                       "This is an average result.\n"
+                    "February 2011. The device is working on an Android OS, v2.1 (Eclair) " \
+                    "with a 420 MHz processor. Alcatel OT-890 has 150 MB of built-in memory. " \
+                    "This device has a Mediatek MT6516 chipset. The main screen size is 2.8 " \
+                    "inches with 320 x 240 pixels resolution. It has a 143 ppi pixel density. " \
+                    "The screen covers about 38.1% of the device's body. " \
+                    "This is an average result.\n"
 
     p_table = sign_in.find_elements_by_xpath(DashBoard.Products.trows)
     search_product(p_table, p_name, "edit")
@@ -53,7 +53,7 @@ def test_edit_product(sign_in):
 
 
 def test_delete_product(sign_in):
-    """Функция удаляет первый найденный продукт по заданному названию"""
+    """Успешное удаление первого найденного продукта по заданному названию"""
     p_name = "Alcatel OT-890"
 
     p_table = sign_in.find_elements_by_xpath(DashBoard.Products.trows)
