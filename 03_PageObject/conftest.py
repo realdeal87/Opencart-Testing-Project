f948d0a2b2a673327818d612a65d2ca5c4c88fdb
Andrey <realdeal87@protonmail.com>
2019-10-17 23:16:33 +0300

    Прикрутил логирование прокси сервера, все готово

--- 03_PageObject/conftest.py
+++ 03_PageObject/conftest.py
@@ -1,7 +1,9 @@
 """Модуль предустановок для тестирования сайта Opencart"""
 import datetime
 import logging
+import urllib.parse
 import pytest
+from browsermobproxy import Server, Client
 from selenium import webdriver
 from selenium.webdriver import ChromeOptions, FirefoxOptions
 from selenium.webdriver.support.events import EventFiringWebDriver, AbstractEventListener
@@ -42,18 +44,32 @@ def url(request):
 
 
 @pytest.fixture
-def driver(request):
+def proxy():
+    """Фикстура для конфигурирования прокси-сервера"""
+    server = Server("03_PageObject/browsermob-proxy-2.1.4/bin/browsermob-proxy")
+    server.start()
+    client = Client("localhost:8080")
+    print(client.port)
+    client.new_har()
+    return client
+
+
+@pytest.fixture
+def driver(request, proxy):
     """Фикстура для запуска браузеров {Chrome, Firefox} в полноэкранном режиме"""
     browser = request.config.getoption("--browser")
     waiting = request.config.getoption("--waiting")
+    url = urllib.parse.urlparse(proxy.proxy).path
 
     if browser == "Chrome":
         options = ChromeOptions()
+        options.add_argument('--proxy-server=%s' % url)
         # options.add_argument("--headless")
         # web = webdriver.Chrome(options=options)
         web = EventFiringWebDriver(webdriver.Chrome(options=options), MyListener())
     elif browser == "Firefox":
         options = FirefoxOptions()
+        options.add_argument('--proxy-server=%s' % url)
         # options.add_argument("--headless")
         # web = webdriver.Firefox(options=options)
         web = EventFiringWebDriver(webdriver.Firefox(options=options), MyListener())
@@ -64,8 +80,10 @@ def driver(request):
     web.implicitly_wait(waiting)
 
     def fin():
+        """Файнолайзер: запись логов браузера, прокси сервера, закрытие браузера"""
         if web.name == "chrome":
             browser_logging(web)
+        proxy_logging(proxy)
         web.quit()
 
     request.addfinalizer(fin)
@@ -74,36 +92,42 @@ def driver(request):
 
 @pytest.fixture(scope="session")
 def logging_test():
+    """Фикстура для конфигурирования логирования"""
     # создаём logger
     logger = logging.getLogger("opencart_testing")
     logger.setLevel(logging.DEBUG)
     # создаём консольный handler и задаём уровень
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.WARNING)
+    shr = logging.StreamHandler()
+    shr.setLevel(logging.WARNING)
     # создаём файловый handler и задаём уровень
-    fh = logging.FileHandler("03_PageObject/logs/opencart_testing.log")
-    fh.setLevel(logging.INFO)
+    fhr = logging.FileHandler("03_PageObject/logs/opencart_testing.log")
+    fhr.setLevel(logging.INFO)
     # создаём formatter
     formatter = logging.Formatter('%(asctime)s - %(funcName)s - %(levelname)s - %(message)s')
-    # добавляем formatter в ch и fh
-    ch.setFormatter(formatter)
-    fh.setFormatter(formatter)
-    # добавляем ch и fh к logger
-    logger.addHandler(ch)
-    logger.addHandler(fh)
+    # добавляем formatter в shr и fhr
+    shr.setFormatter(formatter)
+    fhr.setFormatter(formatter)
+    # добавляем shr и fhr к logger
+    logger.addHandler(shr)
+    logger.addHandler(fhr)
     return logger
 
 
 class MyListener(AbstractEventListener):
-    logging.basicConfig(filename="03_PageObject/logs/opencart_testing_webdriver.log", level=logging.ERROR)
+    """Логирование Webdriver"""
+    logging.basicConfig(filename="03_PageObject/logs/opencart_testing_webdriver.log",
+                        level=logging.ERROR)
 
     def before_find(self, by, value, driver):
+        """Логирование перед нахождением элемента"""
         logging.info("Opening element " + by + value)
 
     def after_find(self, by, value, driver):
+        """Логирование после нахождения элемента"""
         logging.info("Find element " + by + value)
 
     def on_exception(self, exception, driver):
+        """Логирование в случае ошибки, создание скриншота"""
         now = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S_")
         message = str(now) + driver.name + "_" + exception.msg
         driver.save_screenshot("03_PageObject/screenshots/" + message[:55] + ".png")
@@ -111,8 +135,18 @@ class MyListener(AbstractEventListener):
 
 
 def browser_logging(driver):
+    """Запись лога браузера"""
     with open("03_PageObject/logs/opencart_testing_browser.log", "a") as file:
         for line in driver.get_log('browser'):
             message = str(line["timestamp"]) + " - " + str(line["source"]) + " - " +\
                 str(line["level"]) + " - " + str(line["message"] + "\n")
+            file.write(message)
+
+
+def proxy_logging(proxy):
+    """Запись лога прокси сервера"""
+    with open("03_PageObject/logs/opencart_testing_proxy.log", "a") as file:
+        params = proxy.har["log"]
+        for key in params:
+            message = str(key) + "->" + str(params[key]) + "\n"
             file.write(message)
--- 03_PageObject/test_is_it_opencart.py
+++ 03_PageObject/test_is_it_opencart.py
@@ -5,12 +5,12 @@ from pageobjects import MainPage
 def test_opencart_mainpage(driver, url, logging_test):
     """Открыть в браузере основную страницу opencart"""
 
-    driver.find_element_by_id("lolka")
-
-    logging_test.debug('debug message')
-    logging_test.info('info message')
-    logging_test.warning('warn message')
-    logging_test.error('error message')
-    logging_test.critical('critical message')
+    # driver.find_element_by_id("lolka")
+    #
+    # logging_test.debug('debug message')
+    # logging_test.info('info message')
+    # logging_test.warning('warn message')
+    # logging_test.error('error message')
+    # logging_test.critical('critical message')
 
     assert MainPage(driver, url).open().title() == "Your Store"
